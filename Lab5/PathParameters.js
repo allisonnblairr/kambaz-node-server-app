6d2c8b866dbc6d8e402fb94e95b5ba6083e9d4ad
allisonnblairr <allyyblairr@gmail.com>
2025-03-06 21:59:53 -0500

    finished up till 5.2.3.4

--- Lab5/PathParameters.js
+++ Lab5/PathParameters.js
@@ -9,5 +9,18 @@ export default function PathParameters(app) {
       const sum = parseInt(a) - parseInt(b);
       res.send(sum.toString());
     });
+    app.get("/lab5/multiply/:a/:b", (req, res) => {
+        const { a, b } = req.params;
+        const sum = parseInt(a) * parseInt(b);
+        res.send(sum.toString());
+    });
+    app.get("/lab5/divide/:a/:b", (req, res) => {
+        const { a, b } = req.params;
+        if (parseInt(b) === 0) {
+            return res.send("Error: Cannot divide by 0.");
+        }
+        const sum = parseInt(a) / parseInt(b);
+        res.send(sum.toString());
+    });
   };
   
--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import Hello from "./Hello.js"
 import Lab5 from "./Lab5/index.js";
+import PathParameters from "./Lab5/PathParameters.js";
+import QueryParameters from './Lab5/QueryParameters.js';
+import WorkingWithObjects from './Lab5/WorkingWithObjects.js';
 
 const app = express()
 PathParameters(app);
+QueryParameters(app);
+WorkingWithObjects(app)
 Lab5(app);
 Hello(app)
 app.listen(process.env.PORT || 4000)
