62915e71631bbdaa4d5953b1d291e272958a1493
allisonnblairr <allyyblairr@gmail.com>
2025-03-20 18:16:01 -0400

    all lab work done

--- index.js
+++ index.js
@@ -4,11 +4,16 @@ import Lab5 from "./Lab5/index.js";
 import PathParameters from "./Lab5/PathParameters.js";
 import QueryParameters from './Lab5/QueryParameters.js';
 import WorkingWithObjects from './Lab5/WorkingWithObjects.js';
+import WorkingWithArrays from './Lab5/WorkingWithArrays.js';
+import cors from "cors";
 
 const app = express()
+app.use(cors());
+app.use(express.json());
 PathParameters(app);
 QueryParameters(app);
-WorkingWithObjects(app)
+WorkingWithObjects(app);
+WorkingWithArrays(app);
 Lab5(app);
-Hello(app)
+Hello(app);
 app.listen(process.env.PORT || 4000)
