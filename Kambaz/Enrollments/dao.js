381c569c97bbf9f1f4ead91b01bf615577761d6b
allisonnblairr <allyyblairr@gmail.com>
2025-04-01 23:06:34 -0400

    finish a6

--- Kambaz/Enrollments/dao.js
+++ Kambaz/Enrollments/dao.js
@@ -1,19 +1,15 @@
-import Database from "../Database/index.js";
-import { v4 as uuidv4 } from "uuid";
-
-export function enrollUserInCourse(userId, courseId) {
-  const { enrollments } = Database;
-  enrollments.push({ _id: uuidv4(), user: userId, course: courseId });
+import model from "./model.js";
+export async function findCoursesForUser(userId) {
+ const enrollments = await model.find({ user: userId }).populate("course");
+ return enrollments.map((enrollment) => enrollment.course);
 }
-export function findEnrollmentsForUser(userId) {
-  const { enrollments } = Database;
-  const userEnrollments = enrollments.filter((enrollment) => enrollment.user === userId);
-  return userEnrollments;
+export async function findUsersForCourse(courseId) {
+ const enrollments = await model.find({ course: courseId }).populate("user");
+ return enrollments.map((enrollment) => enrollment.user);
 }
-export function getEnrollments() {
-    return Database.enrollments;
-  }
-export function unenrollUserFromCourse(userId, courseId) {
-  Database.enrollments = Database.enrollments.filter(e => !(e.user === userId && e.course === courseId));
+export function enrollUserInCourse(user, course) {
+ return model.create({ user, course, _id: `${user}-${course}` });
 }
-  
+export function unenrollUserFromCourse(user, course) {
+ return model.deleteOne({ user, course });
+}
--- Kambaz/Users/routes.js
+++ Kambaz/Users/routes.js
@@ -73,20 +73,26 @@ export default function UserRoutes(app) {
     }
     res.json(currentUser);
   };
-  const findCoursesForEnrolledUser = (req, res) => {
-    let { userId } = req.params;
-    if (userId === "current") {
-      const currentUser = req.session["currentUser"];
-      if (!currentUser) {
-        res.sendStatus(401);
-        return;
-      }
-      userId = currentUser._id;
+  const findCoursesForUser = async (req, res) => {
+    const currentUser = req.session["currentUser"];
+    if (!currentUser) {
+      res.sendStatus(401);
+      return;
     }
-    const courses = courseDao.findCoursesForEnrolledUser(userId);
+    if (currentUser.role === "ADMIN") {
+      const courses = await courseDao.findAllCourses();
+      res.json(courses);
+      return;
+    }
+    let { uid } = req.params;
+    if (uid === "current") {
+      uid = currentUser._id;
+    }
+    const courses = await enrollmentsDao.findCoursesForUser(uid);
     res.json(courses);
-};
-const createCourse = (req, res) => {
+  };
+  app.get("/api/users/:uid/courses", findCoursesForUser);
+  const createCourse = (req, res) => {
     const currentUser = req.session["currentUser"];
     const newCourse = courseDao.createCourse(req.body);
     enrollmentsDao.enrollUserInCourse(currentUser._id, newCourse._id);
@@ -104,9 +110,29 @@ const findEnrollmentsForUser = (req, res) => {
     }
     const enrollments = enrollmentsDao.findEnrollmentsForUser(userId);
     res.json(enrollments);
-};
+ };
+  const enrollUserInCourse = async (req, res) => {
+    let { uid, cid } = req.params;
+    if (uid === "current") {
+      const currentUser = req.session["currentUser"];
+      uid = currentUser._id;
+    }
+    const status = await enrollmentsDao.enrollUserInCourse(uid, cid);
+    res.send(status);
+  };
+  const unenrollUserFromCourse = async (req, res) => {
+    let { uid, cid } = req.params;
+    if (uid === "current") {
+      const currentUser = req.session["currentUser"];
+      uid = currentUser._id;
+    }
+    const status = await enrollmentsDao.unenrollUserFromCourse(uid, cid);
+    res.send(status);
+  };
+  app.post("/api/users/:uid/courses/:cid", enrollUserInCourse);
+  app.delete("/api/users/:uid/courses/:cid", unenrollUserFromCourse);
   app.post("/api/users/current/courses", createCourse);
-  app.get("/api/users/:userId/courses", findCoursesForEnrolledUser);
+  app.get("/api/users/:uid/courses", findCoursesForUser);
   app.get("/api/users/:userId/enrollments", findEnrollmentsForUser);
   app.post("/api/users", createUser);
   app.get("/api/users", findAllUsers);
